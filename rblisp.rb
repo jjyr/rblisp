c708fd61ba2cb29bbe321255df10119b2bd4fd08
jjy <jjyruby@gmail.com>
2013-05-20 23:06:55 +0800

    power cli

--- rblisp.rb
+++ rblisp.rb
@@ -3,8 +3,39 @@ require_relative 'core'
 env = new_env
 
 line = 0
+
+def getc
+  if ARGV.size > 0
+    $__file_byte ||= open(ARGV[0])
+    $__file_byte.readchar
+  else
+    STDIN.getc
+  end
+rescue EOFError
+  exit
+end
+
 loop do
   line += 1
-  print "rblisp :#{line} > "
-  p run(gets.chomp, env)
+  print "rblisp :#{line} > " unless ARGV.size > 0
+  count = 0
+  str = ""
+  while char = getc
+    if char == '('
+      count += 1
+    elsif char == ')'
+      count -= 1
+    elsif char =~ /\s/
+      char = ' '
+    elsif char == nil
+      exit
+    end
+    str << char
+    if count == 0
+      str.strip!
+      next if str.empty?
+      p run(str, env) 
+      break
+    end
+  end
 end
