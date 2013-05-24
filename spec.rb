45320fcb9e9015e08e6ff0324c3922934e7c33e0
jjy <jjyruby@gmail.com>
2013-05-24 22:11:06 +0800

    fix some tests

--- spec.rb
+++ spec.rb
@@ -12,19 +12,18 @@ describe "rblist" do
     run("(+ 2 2)").should == 4
     run("(+ (* 2 100) (* 1 10))").should == 210
     run("(define x 3)").should == nil
-    run("x").should == :x
+    run("(quote x)").should == :x
     run("exit").class.should == Method
     run("(puts nil)").should == nil
 
     e = new_env
     run("(define x 5)", e).should == nil
-    run("(x 5 6 7)", e).should == [5,5,6,7]
-    run("(x 5 6 7)", e).should == run("(list x 5 6 7)", e)
+    run("(list x 5 6 7)", e).should == [5,5,6,7]
     run("x", e).should == 5
 
     run("(quote 'a')").should == 'a'
     run("(quote a)").should == :a
-    run("(1 2 5 (+ 3 5))").should == [1,2,5,8]
+    run("(list 1 2 5 (+ 3 5))").should == [1,2,5,8]
     run("(cond ((atom 6) (+ 2 3)) ((atom (car (b 5 9))) (+ 2 2)))").should == 4
     run("((lambda (x) (+ x x)) 5)").should == 10
 
@@ -52,8 +51,10 @@ describe "rblist" do
     run("(not (and (and true true) (or (and true false) true)))").should == false
     run("(and true true true false true)").should == false
     run("(or false false true false true)").should == true
-    run("(map (lambda (x) (* 2 x)) (2 5 4 6))").should == [4, 10, 8, 12]
-    run("(string (map (lambda (x) (* 2 x)) (2 5 4 6)))").should == "410812"
+    run("(and true #t)").should == true
+    run("(and #f #t)").should == false
+    run("(map (lambda (x) (* 2 x)) (list 2 5 4 6))").should == [4, 10, 8, 12]
+    run("(string (map (lambda (x) (* 2 x)) (list 2 5 4 6)))").should == "410812"
 
     e = new_env
     run("(define twice (lambda (x) (* 2 x)))", e).should == nil
