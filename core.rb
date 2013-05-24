02cb093f5113d36972e20d12c5c54d7804ccfa13
jjy <jjyruby@gmail.com>
2013-05-25 00:09:40 +0800

    support let!!

--- core.rb
+++ core.rb
@@ -270,6 +270,9 @@ def evaluate token, env = new_env
     #{arr[1].map{|argm| "env[:#{argm}] = #{argm.to_s}"}.join ";"}
     #{eval_str arr[2]}
     }"
+  when :let
+    let, var_exprs, expr = arr
+    evaluate [[:lambda, var_exprs.map(&:first), expr], * var_exprs.map(&:last)], env
   when :if
     _if, cond, t_tokens, el_token = arr
     evaluate [:cond, [cond, t_tokens], [true, el_token]], env.new_stack
